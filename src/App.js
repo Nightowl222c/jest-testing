e0a052d5097bb2a6c61551413ef3636e92fe68ef
Colin <colin.zimmermann@renuo.ch>
2023-04-04 15:13:03 +0200

    added ui fizzbuzz

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { fizzBuzz } from './FizzBuzz/fizzBuzz';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Testing with React</h2>
+function App() {
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState([]);
+
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        const fizzBuzzResult = fizzBuzz(Number(input));
+        setResult(fizzBuzzResult);
+    };
+
+    return (
+        <div className="fizzbuzz-container">
+            <div>
+                <label htmlFor="input">Geben Sie eine Zahl ein:</label>
+                <div>
+                    <input type="number" id="input" value={input} onChange={handleChange} />
+                    <button onClick={handleSubmit}>FizzBuzz</button>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
             </div>
-        );
-    }
+                <ul className="fizzbuzz-list">
+                    {result.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
+    );
 }
 
 export default App;
--- src/FizzBuzz/fizzBuzz.js
+++ src/FizzBuzz/fizzBuzz.js
@@ -1,25 +1,27 @@
-//Anleitung FizzBuzz
+//Anleitung FizzBuzz:
 //Für Zahlen, die durch 3 teilbar sind, wird "Fizz" ausgegeben.
 //Für Zahlen, die durch 5 teilbar sind, wird "Buzz" ausgegeben.
 //Für Zahlen, die sowohl durch 3 als auch durch 5 teilbar sind, wird "FizzBuzz" ausgegeben.
 //Zum Beispiel wäre die Sequenz von 1 bis 15:
 
-export function fizzBuzz () {
+export function fizzBuzz(input) {
     const result = [];
-    for (let i = 1; i <= 100; i++) {
+
+    for (let i = 1; i <= input; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            result.push('FizzBuzz');
-            console.log('FizzBuzz');
+            result.push("FizzBuzz");
+            console.log("FizzBuzz");
         } else if (i % 3 === 0) {
-            result.push('Fizz');
-            console.log('Fizz');
+            result.push("Fizz");
+            console.log("Fizz");
         } else if (i % 5 === 0) {
-            result.push('Buzz');
-            console.log('Buzz');
+            result.push("Buzz");
+            console.log("Buzz");
         } else {
             result.push(i.toString());
             console.log(i);
         }
     }
+
     return result;
 }
