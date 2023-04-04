3edd8e6cd4aaa6021113f0fec433c3469a48b7f9
Colin <colin.zimmermann@renuo.ch>
2023-04-04 12:36:25 +0200

    added fizzBuzz bdd syntax

--- src/FizzBuzz/fizzBuzzTest.test.js
+++ src/FizzBuzz/fizzBuzzTest.test.js
@@ -1,28 +1,20 @@
-import fizzBuzz from '../FizzBuzz/fizzBuzz';
+import { fizzBuzz } from './fizzbuzz';
 
-describe('fizzBuzz function', () => {
-    it('outputs the correct sequence of FizzBuzz values', () => {
-        // Create an array with the expected output values
-        const expectedOutput = [
-            1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz',
-            16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'FizzBuzz',
-            31, 32, 'Fizz', 34, 'Buzz', 'Fizz', 37, 38, 'Fizz', 'Buzz', 41, 'Fizz', 43, 44, 'FizzBuzz',
-            46, 47, 'Fizz', 49, 'Buzz', 'Fizz', 52, 53, 'Fizz', 'Buzz', 56, 'Fizz', 58, 59, 'FizzBuzz',
-            61, 62, 'Fizz', 64, 'Buzz', 'Fizz', 67, 68, 'Fizz', 'Buzz', 71, 'Fizz', 73, 74, 'FizzBuzz',
-            76, 77, 'Fizz', 79, 'Buzz', 'Fizz', 82, 83, 'Fizz', 'Buzz', 86, 'Fizz', 88, 89, 'FizzBuzz',
-            91, 92, 'Fizz', 94, 'Buzz', 'Fizz', 97, 98, 'Fizz', 'Buzz'
-        ];
+//The 1th element is the 0th element because javascript is 0 based.
+test('returns "1" for the number 1', () => {
+    expect(fizzBuzz()[0]).toEqual('1');
+});
 
-        // Redirect console.log to an array for testing
-        const consoleOutput = [];
-        const consoleLog = console.log;
-        console.log = (value) => consoleOutput.push(value);
+//The 14th element is the 15th element because javascript is 0 based.
+//For numbers that are divisible by both 3 and 5, "FizzBuzz" is output.
+test('returns "FizzBuzz" for the number 15', () => {
+    expect(fizzBuzz()[14]).toEqual('FizzBuzz');
+});
 
-        // Call the function and check the output
-        fizzBuzz();
-        expect(consoleOutput).toEqual(expectedOutput);
+test('returns "Fizz" for the number 3', () => {
+    expect(fizzBuzz()[2]).toEqual('Fizz');
+});
 
-        // Restore console.log
-        console.log = consoleLog;
-    });
+test('returns "Buzz" for the number 5', () => {
+    expect(fizzBuzz()[4]).toEqual('Buzz');
 });
